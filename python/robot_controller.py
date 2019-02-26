3bb5b2ffe718bc29972f12edd82349aed948f30f
HekpoMaH <dobrikg666@gmail.com>
2019-02-26 21:11:32 +0000

    Some stub generated

--- python/robot_controller.py
+++ python/robot_controller.py
@@ -13,16 +13,169 @@ from geometry_msgs.msg import Twist
 # Laser scan message:
 # http://docs.ros.org/api/sensor_msgs/html/msg/LaserScan.html
 from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import JointState
 # For groundtruth information.
 from gazebo_msgs.msg import ModelStates
 from tf.transformations import euler_from_quaternion
+from tf import TransformListener
+from obstacle_avoidance import SimpleLaser
+from geometry_msgs.msg import PoseStamped
+import scipy.signal
+import matplotlib.pylab as plt
+
+FREE = 0
+UNKNOWN = 1
+OCCUPIED = 2
+
+ROBOT_RADIUS = 0.105 / 2.
+
+X = 0
+Y = 1
+YAW = 2
+
+class OccupancyGrid(object):
+  def __init__(self, values, origin, resolution):
+    self._original_values = values.copy()
+    self._values = values.copy()
+    # Inflate obstacles (using a convolution).
+    inflated_grid = np.zeros_like(values)
+    inflated_grid[values == OCCUPIED] = 1.
+    w = 2 * int(ROBOT_RADIUS / resolution) + 1
+    inflated_grid = scipy.signal.convolve2d(inflated_grid, np.ones((w, w)), mode='same')
+    self._values[inflated_grid > 0.] = OCCUPIED
+    self._origin = np.array(origin[:2], dtype=np.float32)
+    self._origin -= resolution / 2.
+    assert origin[YAW] == 0.
+    self._resolution = resolution
+
+  @property
+  def values(self):
+    return self._values
+
+  @property
+  def resolution(self):
+    return self._resolution
+
+  @property
+  def origin(self):
+    return self._origin
+
+  def draw(self):
+    plt.imshow(self._original_values.T, interpolation='none', origin='lower',
+               extent=[self._origin[X],
+                       self._origin[X] + self._values.shape[0] * self._resolution,
+                       self._origin[Y],
+                       self._origin[Y] + self._values.shape[1] * self._resolution])
+    plt.set_cmap('gray_r')
+
+  def get_index(self, position):
+    idx = ((position - self._origin) / self._resolution).astype(np.int32)
+    if len(idx.shape) == 2:
+      idx[:, 0] = np.clip(idx[:, 0], 0, self._values.shape[0] - 1)
+      idx[:, 1] = np.clip(idx[:, 1], 0, self._values.shape[1] - 1)
+      return (idx[:, 0], idx[:, 1])
+    idx[0] = np.clip(idx[0], 0, self._values.shape[0] - 1)
+    idx[1] = np.clip(idx[1], 0, self._values.shape[1] - 1)
+    print('idx=', idx)
+    return tuple(idx)
+
+  def get_position(self, i, j):
+    return np.array([i, j], dtype=np.float32) * self._resolution + self._origin
+
+  def is_occupied(self, position):
+    return self._values[self.get_index(position)] == OCCUPIED
+
+  def is_free(self, position):
+    return self._values[self.get_index(position)] == FREE
+
+
+class LeaderPos(object):
+    # TODO I Imagine getting the state/position/speed/whatever
+    # through a class like this one
+    # Leader name is so you could share same class for each robot
+    # You 'might' be able to use different callback fns in the same class
+    def __init__(self, leader_name):
+        rospy.Subscriber('/'+leader_name+'/joint_states', JointState, self.callback)
+    def callback(self, msg):
+        print(msg)
+        
+class SLAM(object):
+  def __init__(self):
+    rospy.Subscriber('/map', OccupancyGrid, self.callback)
+    self._tf = TransformListener()
+    self._occupancy_grid = None
+    self._pose = np.array([np.nan, np.nan, np.nan], dtype=np.float32)
+    
+  def callback(self, msg):
+    values = np.array(msg.data, dtype=np.int8).reshape((msg.info.width, msg.info.height))
+    processed = np.empty_like(values)
+    processed[:] = FREE
+    processed[values < 0] = UNKNOWN
+    processed[values > 50] = OCCUPIED
+    processed = processed.T
+    origin = [msg.info.origin.position.x, msg.info.origin.position.y, 0.]
+    resolution = msg.info.resolution
+    self._occupancy_grid = OccupancyGrid(processed, origin, resolution)
+
+  def update(self):
+    # Get pose w.r.t. map.
+    a = 'occupancy_grid'
+    b = 'base_link'
+    if self._tf.frameExists(a) and self._tf.frameExists(b):
+      try:
+        t = rospy.Time(0)
+        position, orientation = self._tf.lookupTransform('/' + a, '/' + b, t)
+        self._pose[X] = position[X]
+        self._pose[Y] = position[Y]
+        _, _, self._pose[YAW] = euler_from_quaternion(orientation)
+      except Exception as e:
+        print(e)
+    else:
+      print('Unable to find:', self._tf.frameExists(a), self._tf.frameExists(b))
+    pass
+
+  @property
+  def ready(self):
+    return self._occupancy_grid is not None and not np.isnan(self._pose[0])
+
+  @property
+  def pose(self):
+    return self._pose
+
+  @property
+  def occupancy_grid(self):
+    return self._occupancy_grid
+
+#Not sure if we need this guy
+class GoalPose(object):
+  def __init__(self):
+    rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.callback)
+    self._position = np.array([np.nan, np.nan], dtype=np.float32)
+
+  def callback(self, msg):
+    # The pose from RViz is with respect to the "map".
+    self._position[X] = msg.pose.position.x
+    self._position[Y] = msg.pose.position.y
+    print('Received new goal position:', self._position)
+
+  @property
+  def ready(self):
+    return not np.isnan(self._position[0])
+
+  @property
+  def position(self):
+    return self._position
 
 def run():
-    rospy.init_node('robot_controller')
+    rospy.init_node('follower_1_controller')
+    publisher = rospy.Publisher('/tb3_1/cmd_vel', Twist, queue_size=5)
+    leader = LeaderPos('tb3_0')
+    laser = SimpleLaser('tb3_1')
     rate_limiter = rospy.Rate(100)
 
     while not rospy.is_shutdown():
-        print("WAT")
-    pass
+        if not laser.ready:
+            rate_limiter.sleep()
+            continue
 if __name__ == '__main__':
     run()
